40b0b945e72feae34f55838a3c2a8d77e3926463
Carlos Oporto <carlosoporto@gmail.com>
2022-10-03 18:51:36 -0500

    Update metadata.js

--- utils/metadata.js
+++ utils/metadata.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 
-for(var i = 0; i <10; i++) {
+for(var i = 1; i <11; i++) {
     var json = {}
     json.name = "Crazy Food #" + i;
-    json.description = "Crazy Foods vol.1 is an epic collection of 5.000 art pieces. Each one represents the food variety this world has to offer. These foods will bring out your inner madness. Are you ready? This is Crazy Food #" + i;
+    json.description = "Crazy Foods vol.1 is an epic collection of 7.500 art pieces. Each one represents the food variety this world has to offer. These foods will bring out your inner madness. Are you ready? This is Crazy Food #" + i;
     json.image = "ipfs://bafybeic455qzhnnbntwzn4amh5d465axby5llpuqkwpdyz6w6f54thtu2e/" + i + ".png";
-    json.artist = "Wunbit"
+    json.artist = "Wunbit";
+    json.external_url = "https://wunbit.com",
 
-    fs.writeFileSync('' + i, JSON.stringify(json));
+    fs.writeFileSync('' + i + '.json', JSON.stringify(json, null, 5));
 }
