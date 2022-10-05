b55aa30d09581ce64a6890f89a88fe4a8e676fb9
Carlos Oporto <carlosoporto@gmail.com>
2022-10-04 20:44:26 -0500

    Update metadata.js

--- utils/metadata.js
+++ utils/metadata.js
@@ -4,35 +4,50 @@ Array.prototype.random = function () {
   return this[Math.floor((Math.random()*this.length))];
 }
 
+var attribute_origin = fs.readFileSync('./attributes_list/origin.csv').toString().split("\n");
+for(i in attribute_origin) {
+    //console.log(attribute_origin[i]);
+}
+
+var attribute_food = fs.readFileSync('./attributes_list/food.csv').toString().split("\n");
+for(i in attribute_food) {
+    //console.log(attribute_food[i]);
+}
+
 for(var i = 1; i <5; i++) {
     var json = {}
-    json.name = "Crazy Food #" + i;
-    json.description = "Crazy Foods vol.1 is an epic collection of 7.500 art pieces. Each one represents the food variety this world has to offer. These foods will bring out your inner madness. Are you ready? This is Crazy Food #" + i;
-    json.image = "ipfs://bafybeic455qzhnnbntwzn4amh5d465axby5llpuqkwpdyz6w6f54thtu2e/" + i + ".png";
+    json.name = "Crazy Food #" + [i-1];
+    json.description = "Crazy Foods vol.1 is an epic collection of 7.500 art pieces. Each one represents the food variety this world has to offer. These foods will bring out your inner madness. Are you ready? This is Crazy Food #" + [i-1];
+    json.image = "ipfs://bafybeic455qzhnnbntwzn4amh5d465axby5llpuqkwpdyz6w6f54thtu2e/" + [i-1] + ".png";
     json.artist = "Wunbit";
     json.external_url = "https://wunbit.com",
     json.attributes = [
         {
-          "trait_type": "Background",
-          "value": "Default Background"
+          "trait_type": "Serial",
+          "display_type": "number",
+          "value": i-1
         },
         {
-          "trait_type": "Eyes",
-          "value": "Default Eyes"
+          "trait_type": "Cooking",
+          "value": "Cooking"
         },
         {
           "trait_type": "Feeling",
-          "value": ['Acid','Bitter','Cool','Delicious','Hot','Meaty','Salty','Sour','Sweat','Tasty','Umami','Spicy'].random()
+          "value": "Default Feeling"  
         },
         {
           "trait_type": "Flavor",
-          "value": "Default Flavor"
+          "value": ['Acid','Bitter','Cool','Delicious','Hot','Meaty','Salty','Sour','Sweat','Tasty','Umami','Spicy'].random()
         },
         {
-          "trait_type": "Type",
-          "value": "Default Pickle"
+          "trait_type": "Origin",
+          "value": attribute_origin.random()
+        },
+        {
+          "trait_type": "Food",
+          "value": attribute_food[i-1].trim('\r')
         }
       ],
 
-      fs.writeFileSync('' + i + '.json', JSON.stringify(json, null, 4));
+      fs.writeFileSync('' + i-1 + '.json', JSON.stringify(json, null, 4));
 }
